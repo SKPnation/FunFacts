d51a7b37a26987b7522c3220c445accf4bdd55aa
SKPnation <ayomideseaz@gmail.com>
2018-07-07 18:49:04 +0100

    displays various fun facts in different colours

---
+++ app/src/main/java/com/example/ayomide/funfacts/FunFactsActivity.java
@@ -0,0 +1,50 @@
+package com.example.ayomide.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    // Declare our view variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Assign the Views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = factBook.getFact();
+
+                // Update the screen with our new fact
+                factTextView.setText(fact);
+
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(this, "Confam! Our Activity was created!", Toast.LENGTH_LONG).show();
+        Log.d(TAG,"We're logging e onCreate() method");
+    }
+}
